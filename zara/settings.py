b31b728d443a64b91e44ec28ed1fd51b9089e2ba
kominhyuk <komhgogo@gmail.com>
2021-11-29 19:05:12 +0900

    Modified: feature/settings

--- zara/settings.py
+++ zara/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'zara.wsgi.application'
 DATABASES =  {'default' : 
 {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'zaranara',
-        'USER': 'root',
+        'NAME': os.environ['NAME'],
+        'USER': os.environ['USER'],
         'PASSWORD': os.environ['PASSWORD'],
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'HOST': os.environ['HOST'],
+        'PORT': os.environ['PORT'],
 		'OPTIONS': {'charset': 'utf8mb4'}
     }
 }
