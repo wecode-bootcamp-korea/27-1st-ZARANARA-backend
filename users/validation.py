fbde86e6857741361c6e2b5fa816cfc814e49228
이유진 <youjin@iyujin-ui-MacBookAir.local>
2021-12-04 00:34:38 +0900

    validation.py에validationerror작성,views.py에 적,urls.py 작성

--- users/validation.py
+++ users/validation.py
@@ -1,7 +1,20 @@
 import json,re
+from django.core.exceptions import ValidationError
 
-from django.http.response import JsonResponse
+from django.http import JsonResponse
 
 def email_check(value):
-    if not re.match('^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$', value):
-        return JsonResponse({"ERROR" : "EMAIL_ERROR"}, status = 400)
+    if re.match('^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$', value) is None:
+        raise ValidationError("EMAIL_ERROR")
+
+# # 최소 8자 이상, 문자, 숫자, 특수문자 1개이상
+# def password_check(value):
+#     if not re.match('^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$',value):
+#         return JsonResponse({"ERROR" : "PASSWORD_ERROR"}, status = 400)
+
+
+# # 최소 8자 이상, 문자, 숫자
+def password_check(value):
+    if not re.match("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$", value):
+        raise ValidationError("PASSWORD_ERROR")
+
--- users/views.py
+++ users/views.py
@@ -1,29 +1,29 @@
-import json,re, bcrypt
+import json, bcrypt
 
-from django.http.response import JsonResponse
+from django.http.response   import JsonResponse
 from django.core.exceptions import ValidationError 
-from django.views import View
+from django.views           import View
 
-from .models      import User
+from users.validation       import email_check, password_check
+
+from .models                import User
 
 class SignUpView(View):
     def post(self, request):
         try:
             data          = json.loads(request.body)
-            user_name     = data['name'],
-            user_email    = data['email'],
+            user_name     = data['name']
+            user_email    = data['email']
             user_password = data['password']
             
-            if not re.match('^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$', user_email):
-                raise ValidationError()
+            email_check(user_email)
 
-            if User.objects.filter(email = user_email).exists:
+            if User.objects.filter(email = user_email).exists():
                 return JsonResponse({"ERROR" : "EMAIL_ALLEADY_EXIST"}, status=400)
         
-            if not re.match("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$", user_password):
-                return JsonResponse({"ERROR" : "PASSWORD_ERROR"}, status = 400) 
+            password_check(user_password)
 
-            hashed_password = bcrypt.hashpw(user_password.encode('utf-8'), bcrypt.gensalt().endcode('urt-8'))
+            hashed_password = bcrypt.hashpw(user_password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
             User.objects.create(
                 name     = user_name,
                 email    = user_email,
@@ -33,3 +33,6 @@ class SignUpView(View):
 
         except KeyError:
             return JsonResponse({"ERROR" : "KEY_ERROR"}, status = 400)
+
+        except ValidationError as v:
+            return JsonResponse({'ERROR' : v.message}, status=400)
