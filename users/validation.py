e81d298b73a012706042b3602053e47a499fe696
이유진 <youjin@iyujin-ui-MacBookAir.local>
2021-12-04 14:47:54 +0900

    validation.py 불필요한import와 주석 삭제

--- users/validation.py
+++ users/validation.py
@@ -1,19 +1,12 @@
-import json,re
-from django.core.exceptions import ValidationError
+import re
 
-from django.http import JsonResponse
+from django.core.exceptions import ValidationError
 
 def email_check(value):
     if re.match('^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$', value) is None:
         raise ValidationError("EMAIL_ERROR")
 
-# # 최소 8자 이상, 문자, 숫자, 특수문자 1개이상
-# def password_check(value):
-#     if not re.match('^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$',value):
-#         return JsonResponse({"ERROR" : "PASSWORD_ERROR"}, status = 400)
-
-
-# # 최소 8자 이상, 문자, 숫자
+# 최소 8자 이상, 문자, 숫자
 def password_check(value):
     if not re.match("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$", value):
         raise ValidationError("PASSWORD_ERROR")
