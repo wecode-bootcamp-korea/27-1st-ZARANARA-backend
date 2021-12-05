a9254da25202a2fc6e1454a30f515d633475aa1d
kominhyuk <komhgogo@gmail.com>
2021-12-05 14:12:36 +0900

    임시저장

--- products/views.py
+++ products/views.py
@@ -6,7 +6,7 @@ from django.http    import JsonResponse
 from .models        import Category, Product, ProductOption, ProductSet, Theme, ThemeProduct
 
 class ProductMainView(View): 
-    def get(self, request): 
+    def get(self, request):
         slide_item = []
         items   = ThemeProduct.objects.select_related('product','theme').filter(theme = Theme.objects.get(id =3))[:3]
         slide_item = [{'slide_id':item.product.id,'slide_image' : item.product.productimage_set.all()[0].url} for item in items]
