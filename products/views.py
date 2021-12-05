62f5093b013479be909bbe2d00588fd4dfbe6a71
Daco <dacokim32@gmail.com>
2021-12-05 15:46:13 +0900

    created : users_like

--- products/views.py
+++ products/views.py
@@ -1,19 +1,22 @@
 import json
 
-from django.http            import JsonResponse
-from django.views           import View
+from django.http        import JsonResponse
+from django.views       import View
 
-from products.models        import Category, Product, ThemeProduct, Theme, Material, ProductImage, ProductOption, Size, Color
+from products.models    import Category, Product, ThemeProduct, Theme, Material, ProductImage, ProductOption, Size, Color
+from users.models       import Like
 
 class DetailView(View):
+    
     def get(self, request, product_id):
         try:
             product_data           = Product.objects.get(id=product_id)
             product_materials_data = product_data.material_set.all()
             product_option_data    = product_data.productoption_set.all() 
             product_images_data    = product_data.productimage_set.all()
+            like_bool              = Like.objects.filter(user=1).exists() #임시로 유저아이디 1번을 지정하였음. 추후 토큰 데코레이터 추가 예정
             
-            product_theme = product_data.themeproduct_set.get(product=product_data.id)
+            product_theme  = product_data.themeproduct_set.get(product=product_data.id)
             theme_products = ThemeProduct.objects.filter(theme=product_theme.id)
 
             material_objects   = [x for x in product_materials_data]
@@ -30,30 +33,29 @@ class DetailView(View):
 
             result_list = [
                 {
-                    "id"             : product_id,
-                    "name"           : product_name,
-                    "price"          : product_price,
-                    "information"    : product_information,
-                    "keyword"        : product_keyword,
-                    "category"       : product_category,
-                    "material"       : [{
-                        "name"       : material.name,
-                        "caution"    : material.caution 
-                    }for material in material_objects],
-                    "detail_image"   : [{
-                        "url"        : image.url,
-                        "alt"        : image.alt
+                    "id"               : product_id,
+                    "name"             : product_name,
+                    "price"            : product_price,
+                    "information"      : product_information,
+                    "keyword"          : product_keyword,
+                    "category"         : product_category,
+                    "users_like"       : like_bool,
+                    "material_name"    : [material.name for material in material_objects],
+                    "material_caution" : [material.caution for material in material_objects],
+                    "detail_image"     : [{
+                        "url"          : image.url,
+                        "alt"          : image.alt
                     }for image in image_objects],
-                    "product_option" : [{
-                        "size"       : option.size.name,
-                        "color"      : option.color.name,
-                        "stock"      : option.stock
+                    "product_option"   : [{
+                        "size"         : option.size.name,
+                        "color"        : option.color.name,
+                        "stock"        : option.stock
                     }for option in option_objects],
-                    "theme_products" : [{
-                        "id"         : theme_product.product.id,
-                        "name"       : theme_product.product.name,
-                        "price"      : theme_product.product.price,
-                        "image_url"  : [x.url for x in theme_product.product.productimage_set.all()]
+                    "theme_products"   : [{
+                        "id"           : theme_product.product.id,
+                        "name"         : theme_product.product.name,
+                        "price"        : theme_product.product.price,
+                        "image_url"    : [x.url for x in theme_product.product.productimage_set.all()]
                     }for theme_product in theme_product_list]
                 }
             ]
