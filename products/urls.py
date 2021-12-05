4595dc767e660230a8a5d1256f4da5351253491e
kominhyuk <komhgogo@gmail.com>
2021-12-05 10:35:21 +0900

    오타 수정

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 
 from products.models import Product
 
-from .views import ProductListView, ProductMainView
+from .views import ProductMainView
 
 urlpatterns = [
     path('', ProductMainView.as_view()),
