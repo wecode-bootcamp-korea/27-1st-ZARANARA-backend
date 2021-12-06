9ebe8c8dd71c5c71db9e12be38a5a25bfb47d693
kominhyuk <komhgogo@gmail.com>
2021-12-06 17:56:54 +0900

    QueryParameter offset, limit생성

--- products/views.py
+++ products/views.py
@@ -7,9 +7,10 @@ from .models        import Theme, ThemeProduct
 
 class ProductSlideView(View): 
     def get(self, request): 
-        slide_item    = []
-        content_limit = request.GET.get('limit','')
-        items         = ThemeProduct.objects.select_related('product','theme').filter(theme = Theme.objects.get(id =3))[:content_limit]
+        slide_item = []
+        offset     = int(request.GET.get('offset', 0))
+        limit      = int(request.GET.get('limit', 3))
+        items      = ThemeProduct.objects.select_related('product','theme').filter(theme = Theme.objects.get(id =3))[offset:limit]
 
         slide_item = [{'slide_id':item.product.id,'slide_image' : item.product.productimage_set.all()[0].url, 'alt' : item.product.productimage_set.all()[0].alt} for item in items]
 
