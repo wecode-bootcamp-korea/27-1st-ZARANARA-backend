a46d9c64419256c0ca0d667f2cede80f88808d87
kominhyuk <komhgogo@gmail.com>
2021-12-06 19:05:33 +0900

    가독성 고려한 수정

--- products/views.py
+++ products/views.py
@@ -12,6 +12,11 @@ class ProductSlideView(View):
         limit      = int(request.GET.get('limit', 3))
         items      = ThemeProduct.objects.select_related('product','theme').filter(theme = Theme.objects.get(id =3))[offset:limit]
 
-        slide_item = [{'slide_id':item.product.id,'slide_image' : item.product.productimage_set.all()[0].url, 'alt' : item.product.productimage_set.all()[0].alt} for item in items]
+        slide_item = [
+            {
+                'slide_id'   : item.product.id,
+                'slide_image': item.product.productimage_set.all()[0].url,
+                'alt'        : item.product.productimage_set.all()[0].alt
+            } for item in items]
 
         return JsonResponse({'slide_item': slide_item}, status = 200)
