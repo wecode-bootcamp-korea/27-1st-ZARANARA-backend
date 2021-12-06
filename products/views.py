d7d1f75b03b971a65f6e13909d2d7fa51fc5f315
kominhyuk <komhgogo@gmail.com>
2021-12-06 16:47:17 +0900

    Query-parameter 적용

--- products/views.py
+++ products/views.py
@@ -7,8 +7,9 @@ from .models        import Theme, ThemeProduct
 
 class ProductSlideView(View): 
     def get(self, request): 
-        slide_item = []
-        items      = ThemeProduct.objects.select_related('product','theme').filter(theme = Theme.objects.get(id =3))[:3]
+        slide_item    = []
+        content_limit = request.GET.get('limit','')
+        items         = ThemeProduct.objects.select_related('product','theme').filter(theme = Theme.objects.get(id =3))[:content_limit]
 
         slide_item = [{'slide_id':item.product.id,'slide_image' : item.product.productimage_set.all()[0].url, 'alt' : item.product.productimage_set.all()[0].alt} for item in items]
 
