f7fec5615c811cace5486b7c30f70134e4430239
kominhyuk <komhgogo@gmail.com>
2021-12-06 13:41:24 +0900

    alt값 추가 반환

--- products/views.py
+++ products/views.py
@@ -6,10 +6,10 @@ from django.http    import JsonResponse
 from .models        import Category, Product, ProductOption, ProductSet, Theme, ThemeProduct
 
 class ProductMainView(View): 
-    def get(self, request):
+    def get(self, request): 
         slide_item = []
         items   = ThemeProduct.objects.select_related('product','theme').filter(theme = Theme.objects.get(id =3))[:3]
-        slide_item = [{'slide_id':item.product.id,'slide_image' : item.product.productimage_set.all()[0].url} for item in items]
+        slide_item = [{'slide_id':item.product.id,'slide_image' : item.product.productimage_set.all()[0].url, 'alt' : item.product.productimage_set.all()[0].alt} for item in items]
 
         list_item = []
         items = Product.objects.select_related('category')\
@@ -18,19 +18,21 @@ class ProductMainView(View):
             product_items = ProductSet.objects.filter(product_set = item)
             list_item.append(
                 {
-                    'set_image'    : item.productimage_set.all()[0].url,
-                    'set_item': [
+                    'set_id'   : item.id,
+                    'set_image': item.productimage_set.all()[0].url,
+                    'set_alt' : item.productimage_set.all()[0].alt,
+                    'set_item' : [
                         {
                             'id'        : product_item.product.id,
                             'name'      : product_item.product.name,
                             'keyword'   : product_item.product.keyword,
                             'price'     : product_item.product.price,
                             'x_position': product_item.x_position,
-                            'y_position': product_item.y_position
+                            'y_position': product_item.y_position,
+                            'alt' : product_item.product.productimage_set.all()[0].alt
                     } 
                     for product_item in product_items
                     ]
                 }
             )
         return JsonResponse({'slide_item': slide_item, 'list_item': list_item}, status = 200)
-
