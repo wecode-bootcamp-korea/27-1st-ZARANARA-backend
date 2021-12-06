1ba6acbb2494628f754ce4a017bd1477f56bdb8e
kominhyuk <komhgogo@gmail.com>
2021-12-06 16:01:30 +0900

    컨벤션에 맞게 views.py 수정

--- products/views.py
+++ products/views.py
@@ -8,7 +8,7 @@ from .models        import Theme, ThemeProduct
 class ProductSlideView(View): 
     def get(self, request): 
         slide_item = []
-        items   = ThemeProduct.objects.select_related('product','theme').filter(theme = Theme.objects.get(id =3))[:3]
+        items      = ThemeProduct.objects.select_related('product','theme').filter(theme = Theme.objects.get(id =3))[:3]
 
         slide_item = [{'slide_id':item.product.id,'slide_image' : item.product.productimage_set.all()[0].url, 'alt' : item.product.productimage_set.all()[0].alt} for item in items]
 
