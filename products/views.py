7ba5ab123fa37093e64a4484ec069463082ff86e
Daco <dacokim32@gmail.com>
2021-12-07 13:53:54 +0900

    created : setproduct-detail-view

--- products/views.py
+++ products/views.py
@@ -12,13 +12,12 @@ class SetProductDetailView(View):
     def get(self, request, product_id):
         try:
             product        = Product.objects.get(id=product_id)
-            product_images     = product.productimage_set.all()
-            image_objects      = [x for x in product_images]
-            sub_products = ProductSet.objects.filter(product_set=product)
-            # like_bool          = Like.objects.filter(user=request.user).exists()
+            product_images = product.productimage_set.all()
+            image_objects  = [x for x in product_images]
+            sub_products   = ProductSet.objects.filter(product_set_id=product)
+            # like_bool    = Like.objects.filter(user=request.user).exists()
 
-            inclueded_product  = [x for x in sub_products]
-            
+            sub_product_list = [x.product for x in sub_products]
             
             result_list = [
                 {
@@ -32,6 +31,17 @@ class SetProductDetailView(View):
                         "url"          : image.url,
                         "alt"          : image.alt
                     }for image in image_objects],
+                    "sub_products"     : [{
+                        "id" : sub_product.id,
+                        "name" : sub_product.name,
+                        "price" : sub_product.price,
+                        "size" : sub_product.productoption_set.all()[0].size.name,
+                        "color" : sub_product.productoption_set.all()[0].color.name,
+                        "sub_product_image" : [{
+                            "url" : sub_product_image.url,
+                            "alt" : sub_product_image.alt
+                        }for sub_product_image in sub_product.productimage_set.all()]
+                    }for sub_product in sub_product_list]
                 }
             ]
 
--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,8 @@
 from django.urls    import path
 
 from users.views    import LoginView, SignUpView
-from products.views import SetProductDetailView
 
 urlpatterns = [
     path('/login', LoginView.as_view()),
-    path('/signup', SignUpView.as_view()),
-    path('/setproducts', SetProductDetailView.as_view),
+    path('/signup', SignUpView.as_view()), 
 ]
