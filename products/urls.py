a3309f4e9157fd98c30fa6b643e0c1c472df4a46
Daco <dacokim32@gmail.com>
2021-12-07 18:51:44 +0900

    created : SetProductView

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views      import ProductView
+from .views      import ProductView, SetProductView
 
 urlpatterns = [
-	path('/detail/<int:product_id>', ProductView.as_view())
+	path('/detail/<int:product_id>', ProductView.as_view()),
+	path('/set-detail/<int:product_id>', SetProductView.as_view())
 ]
--- products/views.py
+++ products/views.py
@@ -3,7 +3,7 @@ import json
 from django.http        import JsonResponse
 from django.views       import View
 
-from products.models    import Category, Product, ThemeProduct, Theme, Material, ProductImage, ProductOption, Size, Color
+from products.models    import Category, Product, ThemeProduct, Theme, Material, ProductImage, ProductOption, Size, Color, ProductSet
 from users.models       import Like, User
 from core.utils         import signin_check_decorator
 
@@ -38,6 +38,40 @@ class ProductView(View):
                     "price"         : product.price,
                     "image_urls"    : [image.url for image in product.productimage_set.all()]
                 }for product in related_products]
+            }
+
+            return JsonResponse({'result':result}, status=200)
+
+        except Product.DoesNotExist:
+            return JsonResponse({'massage':"DoesNotExist"}, status=401)
+
+class SetProductView(View):
+    @signin_check_decorator
+    def get(self, request, product_id):
+        try:
+            product        = Product.objects.get(id=product_id)
+            is_liked       = Like.objects.filter(user=request.user, product=product_id).exists()
+            
+            result = {
+                "id"           : product.id,
+                "name"         : product.name,
+                "price"        : product.price,
+                "information"  : product.information,
+                "keyword"      : product.keyword,
+                "category"     : product.category.name,
+                "is_liked"     : is_liked,
+                "images"       : [{"url" : image.url, "alt" : image.alt} for image in product.productimage_set.all()],
+                "sub_products" : [{
+                    "id"       : product.id,
+                    "name"     : product.name,
+                    "price"    : product.price,
+                    "size"     : product.productoption_set.all()[0].size.name,
+                    "color"    : product.productoption_set.all()[0].color.name,
+                    "images" : [{
+                        "url"   : image.url,
+                        "alt"   : image.alt
+                    }for image in product.productimage_set.all()]
+                } for product.product in ProductSet.objects.filter(product_set_id=product)]
             }
 
             return JsonResponse({'result':result}, status=200)
