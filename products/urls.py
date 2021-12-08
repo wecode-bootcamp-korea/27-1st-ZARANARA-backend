a3c5405b3278731253d76602ea13bb20e5e15c86
Daco <dacokim32@gmail.com>
2021-12-08 10:02:27 +0900

    add : liketoggle

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views      import ProductView,ProductListView, ProductSetListView, SetProductView
+from .views      import ProductView,ProductListView, ProductSetListView, SetProductView, LikeToggleView
 
 urlpatterns = [
     path('', ProductListView.as_view()),
     path('/set',ProductSetListView.as_view()),
 	path('/detail/<int:product_id>', ProductView.as_view()),
-	path('/set-detail/<int:product_id>', SetProductView.as_view())
+	path('/set-detail/<int:product_id>', SetProductView.as_view()),
+	path('/like/<int:product_id>', LikeToggleView.as_view())
 ]
